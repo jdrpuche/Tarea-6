d5af2d8f577f30af8a36a0d59bc6fa57bea4c358
Miguel Aldana Páez <miguel09@ubuntu.(none)>
2013-10-29 00:06:49 -0500

    Modulo graficador

---
+++ plots.py
@@ -0,0 +1,13 @@
+"""
+   Modulo que realiza las graficas de posiciones  para las particulas.
+
+   Input:
+   - Arhivos de entrada que contienen velocidades y posiciones de las particulas
+
+   Output:
+   - Graficas correspondientes a cada archivo
+"""
+import matplotlib
+
+def plot(x,y):
+    plot(x,y)
