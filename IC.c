4532236ea134351d88ad7d7eb422ecd738b6414c
jesus <jesus@jesus-virtual-machine.(none)>
2013-10-28 22:14:23 -0500

    Código definitivo

--- IC.c
+++ IC.c
@@ -8,23 +8,24 @@
 // vz: Velocidad inicial en x (double)
 // vy: velocidad inicial en y (double)
 
-#include <math.h>
 
 #include <stdio.h>
 
 #include <stdlib.h>
 
-#define pi 3.1416
+#include <math.h>
+
+#define PI 3.1416
 
 
-int main void (int argc, char **argv){
+int main (int argc, char **argv){
 
 // Código que asegura que hay cuatro argumentos de entrada en el problema
 
-if(argv!= 4)
+if(argc!= 4)
 {
 
-	printf "El programa necesita más argumentos de entrada";
+	printf ("El programa necesita más argumentos de entrada");
 	exit (1);
 	}
 // definición de las variables de entrada
@@ -45,7 +46,7 @@ y0=atof(argv[2]);
 
 vz=atof(argv[3]);
 
-vy=atof(argv[4]);;
+vy=atof(argv[4]);
 
 // definición de los vectores posición y velocidad
 
@@ -98,72 +99,73 @@ G=1234;
 M=10;
 
 // Método para llenar vectores, se usa un for para llenar las coordenas de cada vector
+int i;
 
 for (i=0;i<12;i++){
 
-X[-1]=x0
+x[-1]=x0;
 
-x[i]= x0+ r1cos((2pi*i)/12);
+x[i]=x0+ r1*cos((2*PI*i)/12);
 
 vx[-1]=vz;
 
-vx[i]= vz + sqrt((G*r1)/M)*sin((2pi*i)/12);
+vx[i]= vz + sqrt((G*M)/r1)*sin((2*PI*i)/12);
 
-y[-1]=y0
+y[-1]=y0;
 
-y[i]= y0 + r1sin((2pi*i)/5);
+y[i]= y0 + r1*sin((2*PI*i)/5);
 
 vj[-1]=vy;
 
-vj[i]=vy + sqrt((G*r1)/M)*cos((2pi*i)/12);
+vj[i]=vy + sqrt((G*M)/r1)*cos((2*PI*i)/12);
 
 }
 for (i=12;i<30;i++)
 {
 
-x[i]= x0 + r2cos((2pi*(i-12)/18);
+x[i]= x0 + r2*cos((2*PI*(i-12)/18));
 
-y[i]= y0 + r2sin((2pi*(i-12)/18);
+y[i]= y0 + r2*sin((2*PI*(i-12)/18));
 
-vx[i]= vz + sqrt((G*r2)/M)*sin((2pi*(i-12))/18);
+vx[i]= vz + sqrt((G*M)/r2)*sin((2*PI*(i-12))/18);
 
-vj[i]= vy + sqrt((G*r2)/M)*cos((2pi*(i-12))/18);
+vj[i]= vy + sqrt((G*M)/r2)*cos((2*PI*(i-12))/18);
 
 }
 
 for (i=30;i<54;i++){
 
-x[i]= x0 + r3cos((2pi*(i-30)/24);
+x[i]= x0 + r3*cos((2*PI*(i-30)/24));
 
-y[i]= y0 + r3cos((2pi*(i-30)/24);
+y[i]= y0 + r3*cos((2*PI*(i-30)/24));
 
-vx[i]= vz + sqrt((G*r3)/M)*sin((2pi*(i-30))/24);
+vx[i]= vz + sqrt((G*M)/r3)*sin((2*PI*(i-30))/24);
 
-vj[i]= vy + sqrt((G*r3)/M)*cos((2pi*(i-30))/24);
+vj[i]= vy + sqrt((G*M)/r3)*cos((2*PI*(i-30))/24);
 
 }
 
 for (i=54;i<84;i++){
 
-x[i]= x0 + r4cos((2pi*(i-54)/30);
+x[i]= x0 + r4*cos((2*PI*(i-54)/30));
 
-y[i]= y0 + r4cos((2pi*(i-54)/30);
+y[i]= y0 + r4*cos((2*PI*(i-54)/30));
 
-vx[i]= vz + sqrt((G*r4)/M)*sin((2pi*(i-54))/30);
+vx[i]= vz + sqrt((G*M)/r4)*sin((2*PI*(i-54))/30);
 
-vj[i]= vy + sqrt((G*r4)/M)*cos((2pi*(i-54))/30);
+vj[i]= vy + sqrt((G*M)/r4)*cos((2*PI*(i-54))/30);
 
 }
 
 for (i=84;i<120;i++){
 
-x[i]= x0 + r0cos((2pi*(i-84)/36);
+x[i]= x0 + r0*cos((2*PI*(i-84)/36));
 
-y[i]= y0 + r0cos((2pi*(i-84)/36);
+y[i]= y0 + r0*cos((2*PI*(i-84)/36));
 
-vx[i]= vz + sqrt((G*r0)/M)*sin((2pi*(i-84))/36);
+vx[i]= vz + sqrt((G*M)/r0)*sin((2*PI*(i-84))/36);
 
-vj[i]= vy + sqrt((G*r0)/M)*cos((2pi*(i-84))/36);
+vj[i]= vy + sqrt((G*M)/r0)*cos((2*PI*(i-84))/36);
 
 }
 return 0;
